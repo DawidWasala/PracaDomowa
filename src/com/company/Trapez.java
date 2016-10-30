92bad6bc140d19d8c495da0a6b4431af492cfccd
BartlomiejRasztabiga <bartlomiej.rasztabiga.official@gmail.com>
2016-10-30 10:31:33 +0100

    Removed file template comments

--- src/com/company/Trapez.java
+++ src/com/company/Trapez.java
@@ -1,9 +1,7 @@
 package com.company;
 
 
-/**
- * Created by Dawid on 2016-10-29.
- */
+
 public class Trapez {
 
     private double sideA;
--- src/com/company/Trojkat.java
+++ src/com/company/Trojkat.java
@@ -1,8 +1,6 @@
 package com.company;
 
-/**
- * Created by Dawid on 2016-10-29.
- */
+
 public class Trojkat {
     private double surface;
     private double height;
